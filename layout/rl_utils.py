c28d139d57f3867187e3f00824c50d298412d1d1
Ian Millington <idmillington@googlemail.com>
2010-04-27 19:03:32 +0100

    Rearranged the order of arguments.

--- layout/rl_utils.py
+++ layout/rl_utils.py
@@ -1,17 +1,16 @@
 import datatypes
 from reportlab.pdfgen.canvas import Canvas
 
-def render_to_reportlab_canvas(rl_canvas, manager, papersize_tuple):
+def render_to_reportlab_canvas(rl_canvas, papersize_tuple, manager):
     """Renders the given manager full size on a page of the given canvas."""
     manager.render(
         datatypes.Rectangle(0, 0, *papersize_tuple),
         dict(output=rl_canvas)
         )
 
-def render_to_reportlab_document(output_filename, manager, papersize_tuple):
+def render_to_reportlab_document(output_filename, papersize_tuple, manager):
     """Creates a complete document in which to render the given manager."""
     c = Canvas(output_filename, papersize_tuple)
-    render_to_reportlab_canvas(c, manager, papersize_tuple)
-    c.showPage()
+    render_to_reportlab_canvas(c, papersize_tuple, manager)
     c.save()
     
