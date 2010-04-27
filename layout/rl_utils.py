be92871d9323440d62f928e652383f4d635f23d5
Ian Millington <idmillington@googlemail.com>
2010-04-27 14:03:49 +0100

    Added utils file for generating reportlab content froma manager.

--- layout/rl_utils.py
+++ layout/rl_utils.py
@@ -1,7 +1,17 @@
 import datatypes
+from reportlab.pdfgen.canvas import Canvas
 
-def render_to_reportlab_canvas(c, manager, papersize_tuple):
-    manager.render(datatypes.Rectangle(0, 0, papersize_tuple), dict(output=c))
+def render_to_reportlab_canvas(rl_canvas, manager, papersize_tuple):
+    """Renders the given manager full size on a page of the given canvas."""
+    manager.render(
+        datatypes.Rectangle(0, 0, papersize_tuple),
+        dict(output=rl_canvas)
+        )
 
-def render_to_reportlab_document(filename, manager, papersize_tuple):
+def render_to_reportlab_document(output_filename, manager, papersize_tuple):
+    """Creates a complete document in which to render the given manager."""
+    c = Canvas(output_filename, papersize_tuple)
+    render_to_reportlab_canvas(c, manager, papersize_tuple)
+    c.showPage()
+    c.save()
     
