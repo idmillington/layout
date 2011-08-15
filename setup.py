3f46b9b0cf1f6e9aa3f15348823089226f3fe5dd
Ian Millington <idmillington@googlemail.com>
2011-08-15 14:15:10 +0100

    Added support for PyPI

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 version = __import__('layout').__version__
 
 setup(
-    name='Layout',
-    version=version,
-    description='Content Layout Engine',
-    author='Ian Millington',
-    author_email='idmillington@googlemail.com',
+    name='layout',
     packages=[
         'layout', 'layout.datatypes', 'layout.elements',
         'layout.managers', 'layout.pages'
         ],
-    requires=['reportlab(>=2.0)']
+
+    version=version,
+    description='Content Layout Engine',
+    author='Ian Millington',
+    author_email='idmillington@googlemail.com',
+
+    url='http://github.com/idmillington/layout',
+    download_url='https://github.com/idmillington/layout/tarball/master',
+
+    keywords=['pdf', 'layout', 'print', 'design'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics",
+        ],
+
+    zip_safe=False
     )
