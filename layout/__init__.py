d254b89baee8c00e63979a5bda2f485b57e1537f
idmillington <idmillington@googlemail.com>
2015-12-25 11:07:35 +0000

    Version bump.

--- layout/__init__.py
+++ layout/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (0,2,5)
+__version_info__ = (0,3,0)
 __version__ = '.'.join([str(__value) for __value in __version_info__])
 __copyright__ = '2006-2015, Ian Millington'
 __license__ = 'MIT'
